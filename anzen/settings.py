7031517d7dd08aaa525a8146d25f7477d8ab95f3
yashmohod <mohodyash@gmail.com>
2022-08-27 23:32:29 +0000

    aug27Live

--- anzen/settings.py
+++ anzen/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-!gu_uhc83u=ka0u=mts5sij$lwv9v)@c^6ya!ey*$=!ym4vf*5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["Grizzliyash.pythonanywhere.com"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Grizzliyash.pythonanywhere.com"]
+# ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -135,17 +135,17 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 LOGIN_REDIRECT_URL = "login/"
 LOGOUT_REDIRECT_URL = "/"
 
-# CSRF_COOKIES_SECURE = True
-# SESSION_COOKIE_SECURE =True
+CSRF_COOKIES_SECURE = True
+SESSION_COOKIE_SECURE =True
 
-# SECURE_HSTS_SECONDS = 15768000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-# SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
 
-# CSP_DEFAULT_SRC = ("'self'",)
-# CSP_STYLE_SRC = ("'self'",)
-# CSP_SCRIPT_SRC = ("'self'",)
-# CSP_IMG_SRC = ("'self'",)
-# CSP_FONT_SRC = ("'self'",)
-# CSP_INCLUDE_NONCE_IN=['script-src','style-src']
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_STYLE_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'",)
+CSP_IMG_SRC = ("'self'",)
+CSP_FONT_SRC = ("'self'",)
+CSP_INCLUDE_NONCE_IN=['script-src','style-src']
